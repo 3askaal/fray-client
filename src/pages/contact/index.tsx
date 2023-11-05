7ced262d2581c2b12bc08f669553f93e574d0b47
Bas Kaal <3askaal@gmail.com>
2023-11-06 00:19:48 +0100

    refactor product page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -54,72 +54,72 @@ export const Contact = () => {
 
   return (
     <div className="contact">
-    <div className="container--text">
-      <h2 className="page-title">Contact</h2>
-      <div className="spacer mt-5">
-        <div>
-          <ul>
-            <li><Instagram className="mr-2" /><a href="https://instagram.com/frayamsterdam">@frayamsterdam</a></li>
-            <li><TelephoneFill className="mr-2" /><a href="tel:0634042924">0634042924</a></li>
-            <li><EnvelopeFill className="mr-2" /><a href="mailto:frayamsterdam@gmail.com">frayamsterdam@gmail.com</a></li>
-          </ul>
-        </div>
+      <div className="container--text">
+        <h2 className="page-title">Contact</h2>
+        <div className="spacer mt-5">
+          <div>
+            <ul>
+              <li><Instagram className="mr-2" /><a href="https://instagram.com/frayamsterdam">@frayamsterdam</a></li>
+              <li><TelephoneFill className="mr-2" /><a href="tel:0634042924">0634042924</a></li>
+              <li><EnvelopeFill className="mr-2" /><a href="mailto:frayamsterdam@gmail.com">frayamsterdam@gmail.com</a></li>
+            </ul>
+          </div>
 
-        <div className="hr"></div>
+          <div className="hr"></div>
 
-        <div>
-          <h4>Blazinbell</h4>
-          <ul>
-            <li>Czaar Peterstraat 263, 1018 PL Amsterdam</li>
-            <li><a href="http://blazinbell.com/">Website</a></li>
-          </ul>
-        </div>
+          <div>
+            <h4>Blazinbell</h4>
+            <ul>
+              <li>Czaar Peterstraat 263, 1018 PL Amsterdam</li>
+              <li><a href="http://blazinbell.com/">Website</a></li>
+            </ul>
+          </div>
 
-        <div>
-          <h4>Independent outlet</h4>
-          <ul>
-            <li>Vijzelstraat 77, 1017 HG Amsterdam</li>
-            <li><a href="https://skateboardsamsterdam.nl/">Website</a></li>
-          </ul>
-        </div>
+          <div>
+            <h4>Independent outlet</h4>
+            <ul>
+              <li>Vijzelstraat 77, 1017 HG Amsterdam</li>
+              <li><a href="https://skateboardsamsterdam.nl/">Website</a></li>
+            </ul>
+          </div>
 
-        <div className="hr"></div>
+          <div className="hr"></div>
 
-        <h3>Commissions</h3>
-        <div className="contact__form">
-          <Row>
-            <Col cols="6">
-              <p>Name</p>
-              <Form.Control name="name" onChange={(value: string) => setCommissionInfo({ ...commissionInfo, name: value })} />
-            </Col>
-            <Col cols="6">
-              <p>Email</p>
-              <Form.Control name="email" onChange={(value: string) => setCommissionInfo({ ...commissionInfo, email: value })} />
-            </Col>
+          <h3>Commissions</h3>
+          <div className="contact__form">
+            <Row>
+              <Col cols="6">
+                <p>Name</p>
+                <Form.Control name="name" onChange={(value: string) => setCommissionInfo({ ...commissionInfo, name: value })} />
+              </Col>
+              <Col cols="6">
+                <p>Email</p>
+                <Form.Control name="email" onChange={(value: string) => setCommissionInfo({ ...commissionInfo, email: value })} />
+              </Col>
 
-            <Col cols="12">
-              <p>Message</p>
-              <Form.Text as="textarea" rows={5} name="message" onChange={(value: string) => setCommissionInfo({ ...commissionInfo, message: value })} />
-            </Col>
-          </Row>
-          <Row className="justify-content-end">
-            <Col cols="5">
-              <Button
-                block
-                type="submit"
-                disabled={!isValid}
-                state={status || 'primary'}
-                className="checkout__submit"
-                onClick={submit}
-              >
-                Send
-              </Button>
-            </Col>
-          </Row>
+              <Col cols="12">
+                <p>Message</p>
+                <Form.Text as="textarea" rows={5} name="message" onChange={(value: string) => setCommissionInfo({ ...commissionInfo, message: value })} />
+              </Col>
+            </Row>
+            <Row className="justify-content-end">
+              <Col cols="5">
+                <Button
+                  block
+                  type="submit"
+                  disabled={!isValid}
+                  state={status || 'primary'}
+                  className="checkout__submit"
+                  onClick={submit}
+                >
+                  Send
+                </Button>
+              </Col>
+            </Row>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
 }
 
---
+++ src/pages/product/index.tsx
@@ -0,0 +1,77 @@
+import { useContext, useEffect, useState } from 'react';
+import { Carousel, CarouselItem, Col, Form, Row } from 'react-bootstrap';
+import { useRouter } from 'next/router';
+import { Check, Plus } from 'react-bootstrap-icons';
+import to from 'await-to-js';
+import { useAsyncEffect } from 'rooks';
+const md = require('markdown-it')();
+
+import { useApi } from '@/hooks/useApi';
+import { CartContext } from '@/context/CartContext';
+import { Button } from '@/components';
+import { formatProductPrice } from '@/helpers';
+
+import './product.scss';
+
+export const Product = () => {
+  const { query: { id: productId }} = useRouter()
+  const { get }: any = useApi();
+  const { add, products }: any = useContext(CartContext);
+
+  const [product, setProduct] = useState<any>({});
+  const [selectedSize, setSelectedSize] = useState<any>(null);
+
+  const productInCart = products.find(({ id }: any) => id === product?.id)
+  const sizeOptions = product ? [
+    { value: null, text: '-' },
+    ...Object.keys(product.sizes).map((key) => ({ text: key, value: key }))
+  ] : [];
+
+  useAsyncEffect(async () => {
+    const data: any = await to(get(`products/${productId}`));
+
+    const product = {
+      ...data[1],
+      description: md.render(data[1]?.description || '')
+    };
+
+    setProduct(product.sizes[0] ? formatProductPrice(product) : product)
+  }, [productId])
+
+  return (
+    <div className="product">
+      <div className="product__carousel">
+        <Carousel
+          controls={product.image.data.length > 1}
+          interval={0}
+        >
+          { product.image.data.map((image: any) => (
+            <Carousel.Item key={`slide-${image.url}`}>
+              <img src={image.url} alt={image.url} />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+      <div className="product__details">
+        <h3 className="product__title">{ product.title }</h3>
+        <p className="product__price">&euro;{ product.price }</p>
+        <Form.Select value={selectedSize} onChange={(value) => setSelectedSize(value)}>
+          { sizeOptions.map((option) => (
+            <option key={option.value} value={option.value || ''}>{ option.text }</option>
+          )) }
+        </Form.Select>
+        <p className="product__description body" v-html="product.description" />
+        <Button
+          onClick={() => add({ product, size: selectedSize }) }
+          state={productInCart ? 'success' : !selectedSize ? 'disabled' : null}
+          disabled={productInCart}
+        >
+          { productInCart ? 'Added to cart' : 'Add to cart' }
+          { productInCart ? <Check v-if="productInCart" /> : <Plus v-else /> }
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Product;
